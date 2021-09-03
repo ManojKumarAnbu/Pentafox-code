281919ffa6a34aac3f8ebfde782eb75e4b2822ea
ManojKumarAnbu <90049389+ManojKumarAnbu@users.noreply.github.com>
2021-09-03 19:51:59 +0530

    Update and rename english club-5.py to englishclub-qus5.py

---
+++ englishclub-qus5.py
@@ -0,0 +1,14 @@
+def compare(word1,word2):
+    result=""
+    for i in range(len(word1)):
+        if word1[i] in word2:
+            continue
+        else:
+            result=result+word1[i]
+    return result
+
+word1=input()
+word2=input()
+res1=compare(word1,word2)
+res2=compare(word2,word1)
+print(res1+res2)
